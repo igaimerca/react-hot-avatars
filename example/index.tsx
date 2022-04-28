4ae967082448c874f2db508fc26db5834528f2f7
gasaroleila <61538202+gasaroleila@users.noreply.github.com>
2022-04-28 08:09:26 +0200

    initial boilerplate

---
+++ example/index.tsx
@@ -0,0 +1,16 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Avatar from '../src/index';
+import "./index.css";
+
+const App = () => {
+
+  return (
+    <div className="avatar-container">
+      <Avatar name="aimed" size="300"/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
